bc6d772ed53e99698088c97fd901c0e61f89fcf4
George Song <george@55minutes.com>
2015-10-27 19:03:08 -0700

    Small update to imports

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
-var gulp = require("gulp");
 var gulp_tasks_ts_1 = require("gulp-tasks-ts");
-var environments_ts_1 = require("./src/config/environments.ts");
-gulp_tasks_ts_1.makeSettings(environments_ts_1.default);
+var gulp = require("gulp");
+var environments_1 = require("./src/config/environments");
+gulp_tasks_ts_1.makeSettings(environments_1.default);
 gulp.task("default", ["serve:dev"], () => { });
--- gulpfile.babel.ts
+++ gulpfile.babel.ts
@@ -1,7 +1,7 @@
-import * as gulp from "gulp";
 import {makeSettings} from "gulp-tasks-ts";
+import * as gulp from "gulp";
 
-import environments from "./src/config/environments.ts";
+import environments from "./src/config/environments";
 
 
 makeSettings(environments);
