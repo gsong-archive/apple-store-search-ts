2d708f3d1bea931d0f0cf4103ea3ddff87993769
George Song <george@55minutes.com>
2015-10-27 19:55:23 -0700

    New compiled gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,4 +2,4 @@ var gulp_tasks_ts_1 = require("gulp-tasks-ts");
 var gulp = require("gulp");
 var environments_1 = require("./src/config/environments");
 gulp_tasks_ts_1.makeSettings(environments_1.default);
-gulp.task("default", ["serve:dev"], () => { });
+gulp.task("default", ["serve:dev"], function () { });
